b2b33ed4d7c9ea3821be986510ee6d754fbb0f3f
Frédéric NOËL <contact@fredericnoel.com>
2019-02-18 06:03:32 +0100

    Autoload classes. Appel méthode statique display() de la classe PageAutoInclusion. Tests pour récupération de paramètres d'URL.

--- classes/PageAutoInclusion.php
+++ classes/PageAutoInclusion.php
@@ -4,6 +4,33 @@ class PageAutoInclusion
 {
     public static function displayPage()
     {
+        $page = isset($_GET['page']) ? $_GET['page'] : "";
+        echo $page;
+
+        /*$display = "";
+        $display .= self::getHtml();
+        $display .= self::getHeader();
+        $display .= self::getMain();
+        $display .= self::getFooter();*/
+    }
+
+    /*private static function getHtml($page)
+    {
 
     }
+
+    private static function getHeader()
+    {
+
+    }
+
+    private static function getMain()
+    {
+
+    }
+
+    private static function getFooter()
+    {
+
+    }*/
 }
